986123cadb8d3cee76a008c90a6b7245b77a5d33
iamglenbacs <gpbaculio@gmail.com>
2017-09-23 05:58:05 +0800

    attempt graphql server fix might work

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var graphQLHTTP = require('express-graphql');
 import {schema} from './data/schema'
 module.exports = {
   app: function () {
-    const APP_PORT = 3000;
     const app = express();
     const indexPath = path.join(__dirname, './public/index.html')
     const publicPath = express.static(path.join(__dirname, './public'))
@@ -12,8 +11,11 @@ module.exports = {
     app.use('/public', publicPath)
     app.use('/graphql', graphQLHTTP({graphiql: true, schema, pretty: true})); // graphql endpoint
     app.get('/', function (_, res) { res.sendFile(indexPath) });
+    app.set('port', 4000);
+	let http = require('http');
+	let server = http.createServer(app);
+	server.listen(4000);
     return app;
   }
 }
-
 
