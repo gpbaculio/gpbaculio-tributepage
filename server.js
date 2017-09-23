537ba95ed466530657fa326b3d9bcfdfea1f0bcf
iamglenbacs <gpbaculio@gmail.com>
2017-09-23 08:00:57 +0800

    might work deployed now haha

--- server.js
+++ server.js
@@ -26,5 +26,10 @@ const publicPath = express.static(path.join(__dirname, './public'))
     app.get('/', function (_, res) { res.sendFile(indexPath) });
   let http = require('http');
   let server = http.createServer(app);
-  server.listen(port);
+  app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
 
